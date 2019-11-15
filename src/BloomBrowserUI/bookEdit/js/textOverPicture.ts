0e5747b8723e4483cb92a1f1876f5687d7773fbf
Unknown <jeffrey_su@sil.org>
2019-11-15 15:59:55 -0600

    Enable resize handles on all 4 sides

--- src/BloomBrowserUI/bookEdit/js/textOverPicture.ts
+++ src/BloomBrowserUI/bookEdit/js/textOverPicture.ts
@@ -839,6 +839,7 @@ export class TextOverPictureManager {
         const scale = EditableDivUtils.getPageScale();
 
         textOverPictureElems.resizable({
+            handles: "all",
             stop: (event, ui) => {
                 const target = event.target;
                 if (target) {
